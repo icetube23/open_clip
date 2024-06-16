638a9d5e3f3ca7f7fbb7621130666647e97f4371
Elias Kempf <34234056+icetube23@users.noreply.github.com>
2024-06-16 14:50:28 +0200

    save checkpoints before evaluating

--- src/training/main.py
+++ src/training/main.py
@@ -436,9 +436,6 @@ def main(args):
         train_one_epoch(model, data, loss, epoch, optimizer, scaler, scheduler, dist_model, args, tb_writer=writer)
         completed_epoch = epoch + 1
 
-        if any(v in data for v in ('val', 'imagenet-val', 'imagenet-v2')):
-            evaluate(model, data, completed_epoch, args, tb_writer=writer, tokenizer=tokenizer)
-
         # Saving checkpoints.
         if args.save_logs:
             checkpoint_dict = {
@@ -473,6 +470,9 @@ def main(args):
                 latest_save_path = os.path.join(args.checkpoint_path, LATEST_CHECKPOINT_NAME)
                 torch.save(full_checkpoint_dict, tmp_save_path)
                 os.replace(tmp_save_path, latest_save_path)
+
+        if any(v in data for v in ('val', 'imagenet-val', 'imagenet-v2')):
+            evaluate(model, data, completed_epoch, args, tb_writer=writer, tokenizer=tokenizer)
 
     if args.wandb and is_master(args):
         wandb.finish()
