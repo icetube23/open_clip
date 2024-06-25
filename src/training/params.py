fb1ba9c6b60ea6ada20b3c655d008bc29ddc8fa5
Elias Kempf <34234056+icetube23@users.noreply.github.com>
2024-06-25 11:44:24 +0200

    add cc12m web to params

--- src/training/params.py
+++ src/training/params.py
@@ -61,7 +61,7 @@ def parse_args(args):
     )
     parser.add_argument(
         "--dataset-type",
-        choices=["webdataset", "csv", "synthetic", "cc12m", "auto"],
+        choices=["webdataset", "csv", "synthetic", "cc12m", "cc12m_web", "auto"],
         default="auto",
         help="Which type of dataset to process."
     )
